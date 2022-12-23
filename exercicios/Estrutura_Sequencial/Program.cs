b6f3caf8199e2b9d6a47435a83779d6e1ded0bb1
Eduardo <eduardo@T-480.T-480.org>
2022-12-23 20:47:44 -0300

    Exercicio 01 resolvido

---
+++ exercicios/Estrutura_Sequencial/Program.cs
@@ -0,0 +1,16 @@
+﻿// - Criar um algoritmo que leia um número inteiro e apresente na tela o seu antecedente e o seu sucessor.
+
+using System;
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um numero inteiro.");
+        int num = int.Parse(Console.ReadLine());
+
+        int suc = num + 1;
+        int ant = num - 1;        
+
+        Console.WriteLine($"O numero digitado foi ({num}) seu sucessor é ({suc}) e seu antecessor é ({ant})" );
+    }
+}
